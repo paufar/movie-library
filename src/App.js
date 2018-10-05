ae426da721acb9575669b9746e874a9c3539c7f6
Paula farrugia <paulafarrugia95@gmail.com>
2018-10-05 11:17:10 +0200

    Movie DB API request for top rated movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const API_KEY = 'fea48aa90ea7568ab98e7b21cdb2515a';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { 
+			movies: [],
+			pageNum: 1
+		};
+
+		this.fetchMovies();
+	}
+
+	fetchMovies(term){
+		axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en&page=${this.state.pageNum}`)
+		.then(movies => {
+			this.setState({ movies: movies.data.results })
+		})
+	} 
+
+	render() {
+		const movieItems = this.state.movies.map( movie => <div>{movie.title}</div>);
+
+	    return (
+	      <div className="App">
+	      	{ movieItems }
+	      </div>
+	    );
+	}
 }
 
 export default App;
