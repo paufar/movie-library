3e2550d1e58e0d5a0123b0b77cb20c518118c48f
Paula farrugia <paulafarrugia95@gmail.com>
2018-10-09 21:35:59 +0200

    Updates

---
+++ precache-manifest.74d4b41a2693cd1eb84d2b558d30897c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc28652fb898c506a19537acf40e8892",
+    "url": "/movie-library/static/media/movie-placeholder.fc28652f.svg"
+  },
+  {
+    "revision": "ece4b6118c8e68fbddc5ce05580100c9",
+    "url": "/movie-library/static/media/actor-placeholder.ece4b611.svg"
+  },
+  {
+    "revision": "2fc052d8950c7eacfd14",
+    "url": "/movie-library/static/js/runtime~main.2fc052d8.js"
+  },
+  {
+    "revision": "4724f686d6289dc6d536",
+    "url": "/movie-library/static/js/main.4724f686.chunk.js"
+  },
+  {
+    "revision": "2632ac5ed0774d798fde",
+    "url": "/movie-library/static/js/1.2632ac5e.chunk.js"
+  },
+  {
+    "revision": "4724f686d6289dc6d536",
+    "url": "/movie-library/static/css/main.023e8882.chunk.css"
+  },
+  {
+    "revision": "409dc9613f31a3934c0fa16f1ff4ee91",
+    "url": "/movie-library/index.html"
+  }
+];
